9831820f6a1c4482acd100d6fccb232d32e90297
rjohn11118 <145144057+rjohn11118@users.noreply.github.com>
2024-08-21 09:26:10 -0500

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,3 +2,4 @@ let originalColors = ["Red", "Yellow", "Blue", "Yellow"]
 let numItems = originalColors.count
 let uniqueColors = Set(originalColors)
 let uniqueItems = uniqueColors.count
+//print if needed, xcode automatically shows it on side bar so idk if i need to
